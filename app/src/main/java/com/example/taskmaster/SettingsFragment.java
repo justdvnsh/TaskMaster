5e9b2dbfdcf3cda214d7e8e7995b158e5c10b6b9
justdvnsh <justdvnsh2208@gmail.com>
2019-10-30 15:48:10 +0530

    added a preferences and settings activity

---
+++ app/src/main/java/com/example/taskmaster/SettingsFragment.java
@@ -0,0 +1,13 @@
+package com.example.taskmaster;
+
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_settings);
+    }
+}
